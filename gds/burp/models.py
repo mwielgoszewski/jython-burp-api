mwielgoszewski/jython-burp-api

commit c542601fcbb0994455121a14714ef36f0279c320

HttpRequest will always have an HttpResponse object, even if it didn't have one to begin with

gds/burp/models.py
@@ -49,8 +49,8 @@ class HttpRequest(object):
         self.method, self._uri, self.version, self.headers, self.body = \
             _parse_message(self._request)
 
-        if hasattr(messageInfo, 'response'):
-            self.response = HttpResponse(messageInfo, request=self)
+        self.response = HttpResponse(getattr(messageInfo, 'response', None),
+                                     request=self)
 
 
     def __len__(self):
@@ -126,7 +126,7 @@ class HttpResponse(object):
         self.cookies = {}
         self.body = None
 
-        if messageInfo.getResponse():
+        if messageInfo is not None and hasattr(messageInfo, 'response'):
             self._response = messageInfo.getResponse().tostring()
             self.version, self.status_code, self.reason, self.headers, self.body = \
                 _parse_message(messageInfo.getResponse().tostring())

commit 69ed278d9404454d310ac392153dabc1bdf8fcfd

Fixed bug introduced by c542601fcbb0994455121a14714ef36f0279c320 on line 52. Since we already pass the response byte array to HttpResponse, it will not have an attribute called 'response', and thus never actually parse the response properly.

gds/burp/models.py
@@ -119,6 +119,7 @@ class HttpRequest(object):
 
 class HttpResponse(object):
     def __init__(self, messageInfo, request=None):
+        self._response = None
         self.request = request
 
         self.version = None
@@ -129,12 +130,10 @@ class HttpResponse(object):
         self.cookies = {}
         self.body = None
 
-        if messageInfo is not None and hasattr(messageInfo, 'response'):
-            self._response = messageInfo.getResponse().tostring()
+        if messageInfo is not None:
+            self._response = messageInfo.tostring()
             self.version, self.status_code, self.reason, self.headers, self.body = \
-                _parse_message(messageInfo.getResponse().tostring())
-
-        self._messageInfo = messageInfo
+                _parse_message(self._response)
 
 
     def __len__(self):