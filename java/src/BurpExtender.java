0292bcdecc00c889a05b98903eb3c362d701e89c
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2012-05-23 19:01:56 -0400

    commit BurpExtender that loads jython based BurpExtender.py

---
+++ java/src/BurpExtender.java
@@ -0,0 +1,112 @@
+import burp.IBurpExtender;
+import burp.IBurpExtenderCallbacks;
+import burp.IHttpRequestResponse;
+import burp.IScanIssue;
+import burp.StartBurp;
+
+/*
+ * Copyright (c) 2012 Marcin Wielgoszewski.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+/**
+ * @author Marcin Wielgoszewski
+ * @version 1.0
+ *
+ */
+public class BurpExtender
+{
+
+    private static IBurpExtender handler;
+
+    public BurpExtender()
+    {
+        if (handler == null)
+        {
+            BurpExtender.handler = (IBurpExtender) JythonFactory
+                    .getJythonObject(IBurpExtender.class.getName(),
+                            "Lib/BurpExtender.py");
+        }
+    }
+
+    public static IBurpExtender getHandler()
+    {
+        return handler;
+    }
+
+    public static void setHandler(IBurpExtender handle)
+    {
+        handler = handle;
+    }
+
+    public void applicationClosing()
+    {
+        handler.applicationClosing();
+    }
+
+    public void newScanIssue(IScanIssue issue)
+    {
+        handler.newScanIssue(issue);
+    }
+
+    public void processHttpMessage(String toolName, boolean messageIsRequest,
+            IHttpRequestResponse messageInfo)
+    {
+        handler.processHttpMessage(toolName, messageIsRequest, messageInfo);
+    }
+
+    public byte[] processProxyMessage(int messageReference,
+            boolean messageIsRequest, String remoteHost, int remotePort,
+            boolean serviceIsHttps, String httpMethod, String url,
+            String resourceType, String statusCode, String responseContentType,
+            byte[] message, int[] interceptAction)
+    {
+
+        try
+        {
+            return handler.processProxyMessage(messageReference,
+                    messageIsRequest, remoteHost, remotePort, serviceIsHttps,
+                    httpMethod, url, resourceType, statusCode,
+                    responseContentType, message, interceptAction);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return message;
+    }
+
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
+    {
+        handler.registerExtenderCallbacks(callbacks);
+    }
+
+    public void setCommandLineArgs(String[] args)
+    {
+        handler.setCommandLineArgs(args);
+    }
+
+    public static void main(String[] args)
+    {
+        IBurpExtender jyBurpExtender = (IBurpExtender) JythonFactory
+                .getJythonObject(IBurpExtender.class.getName(),
+                        "Lib/BurpExtender.py");
+
+        BurpExtender.setHandler(jyBurpExtender);
+
+        StartBurp.main(args);
+    }
+}
---
+++ java/src/JythonFactory.java
@@ -0,0 +1,47 @@
+import java.io.File;
+
+import org.python.util.PythonInterpreter;
+
+public class JythonFactory
+{
+    private static JythonFactory instance = null;
+
+    public synchronized static JythonFactory getInstance()
+    {
+        if (instance == null)
+            instance = new JythonFactory();
+
+        return instance;
+    }
+
+    public static Object getJythonObject(String interfaceName, String pathToJythonModule)
+    {
+        Object jyObject = null;
+
+        PythonInterpreter interpreter = new PythonInterpreter();
+        interpreter.execfile(pathToJythonModule);
+
+        String tempName = pathToJythonModule.substring(pathToJythonModule.lastIndexOf(File.separator) + 1);
+        tempName = tempName.substring(0, tempName.indexOf("."));
+
+        String instanceName = tempName.toLowerCase();
+        String javaClassName = tempName.substring(0, 1).toUpperCase() + tempName.substring(1);
+
+        String objectDef = "=" + javaClassName + "()";
+
+        interpreter.exec(instanceName + objectDef);
+
+        try
+        {
+            Class JavaInterface = Class.forName(interfaceName);
+            jyObject = interpreter.get(instanceName).__tojava__(JavaInterface);
+        }
+        catch (ClassNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return jyObject;
+    }
+
+}
