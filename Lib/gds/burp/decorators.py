622e53ee10a7a007013cb796f81c900bff64914a
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2012-05-26 22:37:16 -0400

    copy reify decorator from Pyramid project for lazy construction of various class attributes

--- Lib/gds/burp/decorators.py
+++ Lib/gds/burp/decorators.py
@@ -36,3 +36,23 @@ class callback(object):
         else:
             return types.MethodType(obj._check_and_callback, self.func, parent)
 
+
+class reify(object):
+    '''
+    Put the result of a method which uses this (non-data)
+    descriptor decorator in the instance dict after the first call,
+    effectively replacing the decorator with an instance variable.
+    '''
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        try:
+            self.__doc__ = wrapped.__doc__
+        except: # pragma: no cover
+            pass
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
