5fb47fd17bd250ad811955e07b039cb9fc670a17
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2012-06-25 13:43:05 -0400

    correct spelling

--- Lib/gds/burp/dispatchers.py
+++ Lib/gds/burp/dispatchers.py
@@ -38,84 +38,84 @@ class PluginDispatcher(Component):
 
     intruderRequest = OrderedExtensionsOption('plugins', 'intruder.request',
          IIntruderRequestHandler, None, False,
-         '''List of components implmenting the `IIntruderRequestHandler`,
+         '''List of components implementing the `IIntruderRequestHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP requests directly before Burp Intruder
          sends it on the wire.''')
 
     intruderResponse = OrderedExtensionsOption('plugins', 'intruder.response',
         IIntruderResponseHandler, None, False,
-         '''List of components implmenting the `IIntruderResponseHandler`,
+         '''List of components implementing the `IIntruderResponseHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP responses directly after Burp Intruder
          receives if off the wire.''')
 
     proxyRequest = OrderedExtensionsOption('plugins', 'proxy.request',
          IProxyRequestHandler, None, False,
-         '''List of components implmenting the `IProxyRequestHandler`,
+         '''List of components implementing the `IProxyRequestHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP requests directly before Burp Proxy
          sends it on the wire.''')
 
     proxyResponse = OrderedExtensionsOption('plugins', 'proxy.response',
         IProxyResponseHandler, None, False,
-         '''List of components implmenting the `IProxyResponseHandler`,
+         '''List of components implementing the `IProxyResponseHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP responses directly after Burp Proxy
          receives if off the wire.''')
 
     repeaterRequest = OrderedExtensionsOption('plugins', 'repeater.request',
          IRepeaterRequestHandler, None, False,
-         '''List of components implmenting the `IRepeaterRequestHandler`,
+         '''List of components implementing the `IRepeaterRequestHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP requests directly before Burp Repeater
          sends it on the wire.''')
 
     repeaterResponse = OrderedExtensionsOption('plugins', 'repeater.response',
         IRepeaterResponseHandler, None, False,
-         '''List of components implmenting the `IRepeaterResponseHandler`,
+         '''List of components implementing the `IRepeaterResponseHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP responses directly after Burp Repeater
          receives if off the wire.''')
 
     scannerRequest = OrderedExtensionsOption('plugins', 'scanner.request',
          IScannerRequestHandler, None, False,
-         '''List of components implmenting the `IScannerRequestHandler`,
+         '''List of components implementing the `IScannerRequestHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP requests directly before Burp Scanner
          sends it on the wire.''')
 
     scannerResponse = OrderedExtensionsOption('plugins', 'scanner.response',
         IScannerResponseHandler, None, False,
-         '''List of components implmenting the `IScannerResponseHandler`,
+         '''List of components implementing the `IScannerResponseHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP responses directly after Burp Scanner
          receives if off the wire.''')
 
     sequencerRequest = OrderedExtensionsOption('plugins', 'sequencer.request',
          ISequencerRequestHandler, None, False,
-         '''List of components implmenting the `ISequencerRequestHandler`,
+         '''List of components implementing the `ISequencerRequestHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP requests directly before Burp Sequencer
          sends it on the wire.''')
 
     sequencerResponse = OrderedExtensionsOption('plugins', 'sequencer.response',
         ISequencerResponseHandler, None, False,
-         '''List of components implmenting the `ISequencerResponseHandler`,
+         '''List of components implementing the `ISequencerResponseHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP responses directly after Burp Sequencer
          receives if off the wire.''')
 
     spiderRequest = OrderedExtensionsOption('plugins', 'spider.request',
          ISpiderRequestHandler, None, False,
-         '''List of components implmenting the `ISpiderRequestHandler`,
+         '''List of components implementing the `ISpiderRequestHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP requests directly before Burp Spider
          sends it on the wire.''')
 
     spiderResponse = OrderedExtensionsOption('plugins', 'spider.response',
         ISpiderResponseHandler, None, False,
-         '''List of components implmenting the `ISpiderResponseHandler`,
+         '''List of components implementing the `ISpiderResponseHandler`,
          in the order in which they will be applied. These components
          handle processing of HTTP responses directly after Burp Spider
          receives if off the wire.''')
