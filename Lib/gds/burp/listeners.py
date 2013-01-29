c3b19feb07552843b31ccac31f4ff82ecae97f69
mwielgoszewski <marcin.wielgoszewski@gmail.com>
2013-01-29 16:57:58 -0500

    added classes to implement new extender api's configuration settings are saved when extension is unloaded/burp is closed PluginListener wraps the PluginDispatcher

---
+++ Lib/gds/burp/listeners.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+'''
+gds.burp.listeners
+~~~~~~~~~~~~~~~~~~
+
+Listeners that implement new Burp Extender API's.
+'''
+from burp import IExtensionStateListener
+from burp import IHttpListener
+
+from .dispatchers import PluginDispatcher
+
+import gds.burp.settings as settings
+
+
+class SaveConfigurationOnUnload(IExtensionStateListener):
+    def __init__(self, burp):
+        self.burp = burp
+        self.log = burp.log
+
+    def saveExtensionSetting(self, name, value):
+        try:
+            self.log.debug('Saving extension setting %s: %r', name, value)
+            self.burp.saveExtensionSetting(name, value)
+        except Exception:
+            self.log.exception('Error saving extension setting %s: %r',
+                               name, value)
+
+    def extensionUnloaded(self):
+        self.saveExtensionSetting(settings.CONFIG_FILENAME[0],
+                                  self.burp.config.filename)
+
+        self.saveExtensionSetting(settings.LOG_LEVEL[0],
+                                  self.burp.log.getEffectiveLevel())
+
+        self.saveExtensionSetting(settings.LOG_FILENAME[0],
+                                  self.burp._handler.baseFilename)
+
+        self.saveExtensionSetting(settings.LOG_FORMAT[0],
+                                  self.burp._handler.formatter._fmt)
+
+        self.burp.issueAlert('Burp extender unloaded...')
+        self.log.debug('Shutting down Burp')
+        return
+
+
+class PluginListener(IHttpListener):
+    def __init__(self, burp):
+        self.burp = burp
+
+    def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
+        toolName = self.burp.getToolName(toolFlag)
+
+        return PluginDispatcher(self.burp).processHttpMessage(
+            toolName, messageIsRequest, messageInfo)
+        
