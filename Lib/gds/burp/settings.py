fefd93296f64bf82c483faf9f9bd1b071c1c54f6
mwielgoszewski <marcin.wielgoszewski@gmail.com>
2013-01-29 16:56:12 -0500

    added settings keys

---
+++ Lib/gds/burp/settings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+gds.burp.settings
+~~~~~~~~~~~~~~~~~
+
+Extension setting keys and default values. Used by :class:`BurpExtender` in
+:meth:`~burp_extender.BurpExtender.saveExtensionSetting` and
+:meth:`~burp_extender.BurpExtender.loadExtensionSetting`.
+'''
+
+CONFIG_FILENAME = ('jython.config.filename', 'burp.ini')
+LOG_FILENAME = ('jython.logging.filename', 'jython-burp.log')
+LOG_FORMAT = ('jython.logging.format', '%(asctime)-15s - %(name)s - %(levelname)s - %(message)s')
+LOG_LEVEL = ('jython.logging.level', 10)
